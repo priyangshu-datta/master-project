31e437126e254e4f3ede9ae0ec1993b0e4259b97
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2024-05-08 20:52:35 +0000

    feature: implementing file uploader plugin

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from icecream import ic
+
 
 st.write('Hello *World*! :sunglasses:')
+
+files = st.file_uploader("Upload Research Paper","pdf",True,)
+
+
+
+for file in files:
+    st.write("filename:", file.name)
+    with open(file.name, "wb") as f:
+        f.write(file.getbuffer())
